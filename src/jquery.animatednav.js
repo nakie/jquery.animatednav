40ff505d5532f2179c47a6158bb92a61d358c66d
Nathan Crews <n_crews@hotmail.com>
2016-02-19 13:29:17 -0600

    Event Conversion
    Move from Timer based to Event based animation control.

--- src/jquery.animatednav.js
+++ src/jquery.animatednav.js
@@ -17,7 +17,10 @@
  	 *
  	**/
 	var animateOut = function( element, $containerElem ){
-		
+			
+		// get child dropdown menu container
+		var $subMenuContainer = $( element ).find(".dropdown-menu");
+
 		// get List items of subMenu List Items
 		var $elems = $( element ).find(".dropdown-menu li");
 
@@ -26,28 +29,56 @@
 
 		// add animate.css Classes to List Items of Main Menu		
 		$mainList.addClass( "animated fadeOut" );
+		
+		/** 
+		 *  better controll of animations 
+		 *  using transition/animation envents.
+		 *  rather than relying on timers.
+		**/
+		// triggers when Last Main menu element Ends its animation.
+		$mainList.last().one( "animationend webkitAnimationEnd oanimationend MSAnimationEnd", function( e ){ 
+			
+			// Add class to dropdown menu to change background
+			// this covers and hides the main menu elements 
+			// AFTER they have faded out.
+			$subMenuContainer.addClass( "animating" );
 
-		// Remove any trace of previous animations
-		$elems.removeClass( "animated fadeOutLeft" ); //***
+			// Loop  subMenu Elements
+			// Attaches animation classes with increasing delay 
+			// to stagger element animations.
+			$elems.each( function( i ) {
 
-		// Loop  subMenu Elements
-		// Attaches animation classes with increasing delay 
-		// to stagger element animations.
-		$elems.each( function( i ) {
-			var $curEle = $( this );
+				// cache current element
+				var $curEle = $( this );
 
-			// addClass ot subMenu LI with Delay
-			setTimeout( function(){
-				//$curEle.removeClass( "fadeOutLeft" ); //***
-				$curEle.addClass( "animated fadeInLeft" );				
-			}, settings.stagger * i );
+				// addClass to subMenu LI with the Delay
+				// so element display is staggered.
+				setTimeout( function(){
+					
+					$curEle.removeClass( "animated fadeOutLeft" ); //
+					$curEle.addClass( "animated fadeInLeft" );
 
-		}); //-END $elems.each()
+				}, settings.stagger * i );
 
-		setTimeout( function(){
-				$mainList.removeClass( "animated fadeOut" );				
-		}, 500 );
+			}); //-END $elems.each()
+	
+			// triggers when First sub menu element Starts animating.
+			$elems.first().one( "animationstart webkitAnimationStart oanimationstart MSAnimationStart", function( e ){ 
+				
+				// clean up animation classes
+				$mainList.removeClass( "animated fadeOut" );	
 
+				// remove this animation Start event listener
+				$elems.first().off( "animationstart webkitAnimationStart oanimationstart MSAnimationStart" );
+					
+			}); //- END $elems.fist().one( animationstart) event listener.
+
+			// remove this animation end event listener
+			$mainList.last().off( "animationend webkitAnimationEnd oanimationend MSAnimationEnd" );
+	
+		 }); //- END $mainList.last.one( animationend ) event listener.
+
+		
 	}; //- END function animateOut()
 
 	/**
@@ -59,10 +90,13 @@
 	 * element: this element of event listener context.
 	 * $containerElem: context for plugin
 	**/
-	var animateIn = function( element, $containerElem ){
-
+	var animateIn = function( element, $containerElem ){	
+		
 		// Get list of menu items that are a dropdown.
 		var $dropdownEle = getOpenDropdown( $containerElem );
+
+		// get child dropdown menu container
+		var $subMenuContainer = $( element ).find(".dropdown-menu");
 
 		// get list items of dropdown menu to be animated
 		var $elems = $( element ).find( ".dropdown-menu li" );
@@ -70,17 +104,17 @@
 		// get top level menu list items.
 		var $mainList = $containerElem.find( "ul.navbar-nav>li" );
 
-		// signal closing of dropdown to prevent to hold off
-		// bootstraps toggle
+		// signal closing of dropdown to hold off bootstraps toggle untill we are ready.
 		$( element ).data( 'closing', true ); 
 
 		// Remove any trace of previous animations
-		$elems.removeClass( "fadeInLeft" ); //***
-
+		$elems.removeClass( "fadeInLeft" );
+		
 		// Loop  subMenu Elements
-		// Attaches animation classes with increasing delay 
-		// to stagger element animations.
+		// Attaches animation classes with increasing delay to stagger element animations.
 		$elems.each( function( i, curElement ) {
+	
+			// cache this element
 			var $curEle = $( curElement );
 			
 			setTimeout( function(){
@@ -88,36 +122,50 @@
 			}, settings.stagger * i );
 
 		}); //-END $elems.each()
-		
-		// Animate Main menu back in
-		// allow time for submenu elements to finish animation.
-		setTimeout( function(){
+	
+		// triggers when last Sub Menu element ends its animation
+		$elems.last().one( "animationend webkitAnimationEnd oanimationend MSAnimationEnd", function( e ){ 
 			
-			
-
+			// Add class to dropdown menu to change background
+			// this covers and hides the main menu elements 
+			// AFTER they have faded out.
+			$subMenuContainer.removeClass( "animating" );
 
 			$mainList.addClass( "animated fadeIn" );						
-			
+
+			// SET closeable flag, signals to the plugin that
+			// the dropdown is now able to be closed.
 			$dropdownEle.data( 'closable', true );
 			
-			// Get element to Toggle
+			// Get dropdown element to Toggle
 			var $toggleElem = $containerElem.find( 'ul.nav li.open>[data-toggle="dropdown"]' );	
 			
 			// trigger toggle on bootstrap dropdown element
 			// this cleans up bootstraps element state.
 			$toggleElem.dropdown( 'toggle' );
 
-			// Signal that the element has been closed.
-			$( element ).data( 'closing', false );
+			// reset clsoing state now that the element has been closed.
+			$( element ).data( 'closing', false );			
 
-		}, $elems.length * 200 );
-
-		// clean up main LI animation classes.
-		setTimeout( function(){
-				$mainList.removeClass( "animated fadeIn" );
+			// triggers when the last Main Menu element ends its animation.
+			$mainList.last().one( "animationend webkitAnimationEnd oanimationend MSAnimationEnd", function( e ){ 
 				
-		}, $elems.length * 500 );
+				// clean up animation classes
+				$mainList.removeClass( "animated fadeIn" );
+	
+				//remove animation end listener.
+				$mainList.last().off( "animationend webkitAnimationEnd oanimationend MSAnimationEnd" );
 
+			}); //- END $mainList.last().one( animationend ) event listener )
+
+			// clean up animation classes
+			$elems.removeClass( "animated fadeOutLeft" );
+
+			//remove animation end listener.
+			$elems.last().off( "animationend webkitAnimationEnd oanimationend MSAnimationEnd" );
+
+		}); //- END $elems.last().one( animationend ) event listener )
+		
 	}; // - END function animateIn()
 
 	/**
@@ -140,15 +188,11 @@
 		
 		// Create breadcrumb container element.
 		var $breadCrumb  = $( '<ol class="breadcrumb "/>' );
-		//$breadCrumbToggle = $( '<li class="active"><a class="toggle-menu active">' + settings.bchometext + '</a></li' );
-		
-		$breadCrumbToggle = $( '<li class="active">' + settings.bchometext + '</li>' );
+				
+		$breadCrumbToggle = $( '<li class="active ">' + settings.bchometext + '</li>' );
 		$breadCrumb.append( $breadCrumbToggle );
 
-		//var $breadCrumb = $containerElem.find( '#breadcrumbContainer'  );
-
-
-       // Get all dropdown toggle elements.
+		// Get all dropdown toggle elements.
         var $toggles = $containerElem.find( "ul.nav li.dropdown .dropdown-toggle" );
 
 		//listen to dropdown-toggle anchors to create breadcrumbs.
@@ -160,13 +204,12 @@
 			$menuToggle = $breadCrumb.find( ".active" );
 
 			$menuToggle.removeClass( "active" );
-			$menuToggle.html( '<a href="#" class="toggle-menu">' + settings.bchometext + '</a>' );
+			$menuToggle.html( '<a href="#" class="toggle-menu ">' + settings.bchometext + '</a>' );
 
 			// Append breadcrumb link
-			$breadCrumb.append( '<li class="active">' + linkText + '</li>' );
+			$breadCrumb.append( '<li class="active animated fadeIn">' + linkText + '</li>' );
 
 		}); // End click ul.nav li.dropdown .dropdown-toggle
-
 				
 		
 		// Breadcrumb click handler.
@@ -197,9 +240,7 @@
 
 	/******************************/
 	/***** Private Properties *****/
-	/******************************/
-
-	
+	/******************************/	
 
     $.fn.animatedNav = function( options ) {
  
@@ -207,7 +248,7 @@
  		// store defaults overridden by options in 
 		// settings variable which lives in plugin scope.
 		// useable as "settings.color"
-		settings = $.extend({}, $.fn.animatedNav.defaults, options );
+		settings = $.extend( {}, $.fn.animatedNav.defaults, options );
 
  		// avoid confusion with "this" context
  		var $containerElem = $( this );
@@ -219,8 +260,6 @@
 			breadcrumbs( $containerElem );			
 
 		} //- END if( settings.breadcrumbs === true )
-
-
 
         // Get list items that are twbs dropdown elements.
         var $dropdowns = $containerElem.find( 'ul.nav .dropdown' );
@@ -241,7 +280,7 @@
 				// Skip animations once the closing event happens.
 				// this prevents an overflow loop as the toggle
 				// in animateIn trigers the hide.bs.dropdown event again
-				if( $( this ).data( 'closing') !== true && bcCheck ){
+				if( $( this ).data( 'closing' ) !== true && bcCheck ){
 					
 					// fire animations sequence.
 					animateIn( this, $containerElem );
@@ -257,29 +296,13 @@
 
 				
 			},
-			"hidden.bs.dropdown" : function() { 
-				// console.log( "hidden.bs.dropdown" );
-				// unused remove later
-			},
 			"show.bs.dropdown": function() { 
 				//console.log( "show.bs.dropdown" );
 
 				animateOut( this, $containerElem );
 				$( this ).data( 'closable', false ); 
-			},
-			"shown.bs.dropdown": function() { 
-
-				// console.log( "shown.bs.dropdown" );
-
-				// Initalize closable to false when the dropdown is shown
-				//$( this ).data( 'closable', false ); 
-				// moved to show.bs.dropdown so the whole shown.bs.dropdown
-				// can be removed hopefully.
-				// unused remove later
-		
 			}
-		});
-
+		}); //- END $dropdowns.on
 
         // return item back for chaining
         return this;
@@ -292,32 +315,8 @@
 		breadcrumb: false,	// flag to use menu breadcrumbs.
 		bchometext: "Main Menu"	// Link text of breadcrumb ( not in use yet. )
 	};
-
  
 }( jQuery ));
 
-
-
-
 ///  END jQuery Plugin.....
 
-
-
-			
-
-			
-
-
-	
-
-			
-			
-
-			
-
-	
-
-
-	
-
-	
